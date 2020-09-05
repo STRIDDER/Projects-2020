aed723f259ada4468e6dc386719f99c33a0bd892
STRIDDER <52578986+STRIDDER@users.noreply.github.com>
2020-09-05 20:37:22 +0530

    Add files via upload

---
+++ Dynamic_Website/Bootstrap/main.js
@@ -0,0 +1,41 @@
+const typedTextSpan = document.querySelector(".typed-text");
+// const cursorSpan = document.querySelector(".cursor");
+const textArray = ["fancy", "better", "modern"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+	//To type a character then pause and wait for typing delay before calling again
+	if (charIndex < textArray[textArrayIndex].length) {
+		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		//erase
+		setTimeout(erase, newTextDelay);
+	}
+}
+function erase() {
+	if (charIndex > 0) {
+		//Means String not entirerly erased yet
+		//Remove character and call array function again
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(
+			0,
+			charIndex - 1
+		); //0 to charIndex-1 value tk chalega
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		//CAll type function in order to call te nest string with more delay
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		//Infinitely looping through strings
+		setTimeout(type, typingDelay + 1100);
+	}
+}
+document.addEventListener("DOMContentLoaded", function () {
+	setTimeout(type, typingDelay + 250);
+});
