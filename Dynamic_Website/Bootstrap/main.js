8ea8a1c549ca7caf342e61fa6f79201ba672aba1
STRIDDER <52578986+STRIDDER@users.noreply.github.com>
2020-09-08 13:51:34 +0530

    The Final Project

--- Dynamic_Website/Bootstrap/main.js
+++ Dynamic_Website/Bootstrap/main.js
@@ -39,3 +39,23 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function () {
 	setTimeout(type, typingDelay + 250);
 });
+
+// The nav bar scroll
+var $document = $(document),
+	$element = $(".navbar"),
+	navbarDefault = "navbar-default";
+navbarTransparent = "navbar-transparent";
+
+fadeInDown = "fadeInDown";
+$document.scroll(function () {
+	if ($document.scrollTop() >= 100) {
+		//user scrolled more than 100 pixel
+		$element.addClass(navbarDefault);
+		$alement.removeClass(navbarTransparent);
+		$element.addClass(fadeInDown);
+	} else {
+		$element.addClass(navbarTransparent);
+		$element.removeClass(navbarDefault);
+		$element.removeClass(fadeInDown);
+	}
+});
